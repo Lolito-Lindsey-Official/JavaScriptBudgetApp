48493439a4b5bfb5fdf3d1188244d08011368caa
Lolito-Lindsey-Official <87788112+Lolito-Lindsey-Official@users.noreply.github.com>
2023-08-01 10:39:50 -0500

    Simplifying the budgeting code
    Just so that it is more representative to my level of front-end understanding.

--- script.js
+++ script.js
@@ -1,113 +1,20 @@
-alert("Welcome to my basic budget app!");
+// Select the necessary HTML elements using their IDs
+let income = document.getElementById('income');
+let expenses = document.getElementById('expenses');
+let calculate = document.getElementById('calculate');
+let output = document.getElementById('output');
 
-/* Grabs all id's from the index.html file */
-let totalAmount = document.getElementById("total-amount");
-let userAmount = document.getElementById("user-amount");
-const checkAmountButton = document.getElementById("check-amount");
-const totalAmountButton = document.getElementById("total-amount-button");
-const productTitle = document.getElementById("product-title");
-const errorMessage = document.getElementById("budget-error");
-const productTitleError = document.getElementById("product-title-error");
-const productCostError = document.getElementById("product-cost-error");
-const amount = document.getElementById("amount");
-const expenditureValue = document.getElementById("expenditure-value");
-const balanceValue = document.getElementById("balance-amount");
-const list = document.getElementById("list");
-/* Sets default amount */
-let tempAmount = 0;
+// Event Listener for Calculate Button
+calculate.addEventListener('click', function() {
+    let totalIncome = Number(income.value); // User's total income
+    let totalExpenses = Number(expenses.value); // User's total expenses
 
-//Set Budget Part
-totalAmountButton.addEventListener("click", () => {
-    tempAmount = totalAmount.value;
-    //empty or negative input
-    if (tempAmount === "" || tempAmount < 0) {
-      errorMessage.classList.remove("hide");
+    let difference = totalIncome - totalExpenses; // Calculate the difference
+
+    // Display the result
+    if(difference >= 0) {
+        output.innerText = "You have a surplus of $" + difference.toFixed(2);
     } else {
-      errorMessage.classList.add("hide");
-      //Set Budget
-      amount.innerHTML = tempAmount;
-      //Set Balance
-      balanceValue.innerText = tempAmount - expenditureValue.innerText;
-      //Clear Input Box
-      totalAmount.value = "";
+        output.innerText = "You have a deficit of $" + Math.abs(difference).toFixed(2);
     }
-  });
-
-  //Function To Disable Edit and Delete Button
-  const disableButtons = (bool) => {
-    let editButtons = document.getElementsByClassName("edit");
-    Array.from(editButtons).forEach((element) => {
-      element.disabled = bool;
-    });
-  };
-
-  //Function To Modify List Elements
-  const modifyElement = (element, edit = false) => {
-    let parentDiv = element.parentElement;
-    let currentBalance = balanceValue.innerText;
-    let currentExpense = expenditureValue.innerText;
-    let parentAmount = parentDiv.querySelector(".amount").innerText;
-    if (edit) {
-      let parentText = parentDiv.querySelector(".product").innerText;
-      productTitle.value = parentText;
-      userAmount.value = parentAmount;
-      disableButtons(true);
-    }
-    balanceValue.innerText = parseInt(currentBalance) + parseInt(parentAmount);
-    expenditureValue.innerText =
-      parseInt(currentExpense) - parseInt(parentAmount);
-    parentDiv.remove();
-  };
-
-  //Function To Create List
-  const listCreator = (expenseName, expenseValue) => {
-    let sublistContent = document.createElement("div");
-    sublistContent.classList.add("sublist-content", "flex-space");
-    list.appendChild(sublistContent);
-    sublistContent.innerHTML = `<p class="product">${expenseName}</p><p class="amount">${expenseValue}</p>`;
-    let editButton = document.createElement("button");
-    editButton.classList.add("fa-solid", "fa-pen-to-square", "edit");
-    editButton.style.fontSize = "1.2em";
-    editButton.addEventListener("click", () => {
-      modifyElement(editButton, true);
-    });
-    let deleteButton = document.createElement("button");
-    deleteButton.classList.add("fa-solid", "fa-trash-can", "delete");
-    deleteButton.style.fontSize = "1.2em";
-    deleteButton.addEventListener("click", () => {
-      modifyElement(deleteButton);
-    });
-    sublistContent.appendChild(editButton);
-    sublistContent.appendChild(deleteButton);
-    document.getElementById("list").appendChild(sublistContent);
-  };
-
-  //Function To Add Expenses
-  checkAmountButton.addEventListener("click", () => {
-    //empty checks
-    if (!userAmount.value || !productTitle.value) {
-      productTitleError.classList.remove("hide");
-      return false;
-    }
-
-    //Enable buttons
-    disableButtons(false);
-
-    //Expense
-    let expenditure = parseInt(userAmount.value);
-
-    //Total expense (existing + new)
-    let sum = parseInt(expenditureValue.innerText) + expenditure;
-    expenditureValue.innerText = sum;
-
-    //Total balance(budget - total expense)
-    const totalBalance = tempAmount - sum;
-    balanceValue.innerText = totalBalance;
-
-    //Create list
-    listCreator(productTitle.value, userAmount.value);
-    
-    //Empty inputs
-    productTitle.value = "";
-    userAmount.value = "";
-  });
+});
