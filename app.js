00230eac838f6c9702c3c6b866e1dfed7df86f1b
lolito-lindsey-official <lolitolindsey@gmail.com>
2024-01-12 13:45:51 -0600

    Added new output errors
    I edited the diction of the output messages for the differences.

--- app.js
+++ app.js
@@ -3,22 +3,52 @@ let income = document.getElementById('income');
 let expenses = document.getElementById('expenses');
 let calculate = document.getElementById('calculate');
 let output = document.getElementById('output');
+// Select additional HTML elements for error messages
+let incomeError = document.getElementById('incomeError');
+let expensesError = document.getElementById('expensesError');
 
 // Event Listener for Calculate Button
 calculate.addEventListener('click', function() {
+
+    // Reset the error messages
+    incomeError.textContent = ' ';
+    expensesError.textContent = ' ';
+
     let totalIncome = Number(income.value); // User's total income
     let totalExpenses = Number(expenses.value); // User's total expenses
+
+        // If the income or expense inputs are empty, let the user know
+        if (income.value.trim() === '' || expenses.value.trim() === '') {
+            output.innerText = "Please enter both an income and expense.";
+            // Exiting the function
+            return;
+        }
+
+    // Cannot have a negative income
+    if (totalIncome < 0 || totalExpenses < 0) {
+        if (totalIncome < 0) {
+            incomeError.innerText = "Cannot have a negative income";
+        }
+    
+        // Cannot have a negative expense
+        if (totalExpenses < 0) {
+            expensesError.innerText = "Cannot have a negative expense";
+        }
+
+        // Exiting the function
+        return;
+    }
 
     let difference = totalIncome - totalExpenses; // Calculate the difference
 
     // Display the result
     if(difference > 0) {
-        output.innerText = "You have a surplus of $" + difference.toFixed(2); 
+        output.innerText = `Well done: You have a budget surplus of ${difference.toFixed(2)}.`; 
     } 
     else if (difference === 0) {
-        output.innerText = `You have a balanced budget.`;
+        output.innerText = `Notice: You have a balanced budget.`;
     }
     else {
-        output.innerText = "You have a deficit of $" + Math.abs(difference).toFixed(2);
+        output.innerText = `Please note: You have a budget deficit of ${Math.abs(difference).toFixed(2)}.`;
     }
 });
