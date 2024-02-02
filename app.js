9aad1d2ea343eed6b7fda16873c70597d5a13de5
lolito-lindsey-official <lolitolindsey@gmail.com>
2024-02-02 09:53:18 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Select the necessary HTML elements using their IDs
 let income = document.getElementById('income');
-let expenses = document.getElementById('expenses');
+let expense = document.getElementById('expenses');
 let calculate = document.getElementById('calculate');
 let output = document.getElementById('output');
 // Select additional HTML elements for error messages
@@ -13,44 +13,39 @@ calculate.addEventListener('click', function() {
     // Reset the error messages
     incomeError.textContent = ' ';
     expensesError.textContent = ' ';
+    output.textContent = ' ';
 
-    let totalIncome = Number(income.value); // User's total income
-    let totalExpenses = Number(expenses.value); // User's total expenses
+    // Convert the string values from the DOM to numeric values
+    let totalIncome = Number(income.value); 
+    let totalExpenses = Number(expense.value);
 
-        // If the income or expense inputs are empty, let the user know
-        if (income.value.trim() === '' || expenses.value.trim() === '') {
-            output.innerText = "Please enter both an income and expense.";
-            // Exiting the function
-            return;
-        }
+    if (income.value.trim() === "" || expense.value.trim() === " ") {
+        output.innerText = "Please fill in all the fields.";
+        return;
+    }
 
     // Cannot have a negative income
-    if (totalIncome < 0 || totalExpenses < 0) {
-        if (totalIncome < 0) {
-            output.innerText = "";
+    if (totalIncome < 0) {
             incomeError.innerText = "Cannot have a negative income";
-        }
+            return;
+    }
     
-        // Cannot have a negative expense
-        if (totalExpenses < 0) {
-            output.innerText = "";
+    // Cannot have a negative expense
+    if (totalExpenses < 0) {     
             expensesError.innerText = "Cannot have a negative expense";
-        }
-
-        // Exiting the function
-        return;
+            return;
     }
 
     let difference = totalIncome - totalExpenses; // Calculate the difference
 
     // Display the result
     if(difference > 0) {
-        output.innerText = `Awesome! You have a budget surplus of ${difference.toFixed(2)}.`; 
+        output.innerText = `You have a budget surplus of ${difference.toFixed(2)}`; 
     } 
     else if (difference === 0) {
-        output.innerText = `Well done. You have a balanced budget.`;
+        output.innerText = `You have a balanced budget.`;
     }
     else {
-        output.innerText = `Please note: You have a budget deficit of ${Math.abs(difference).toFixed(2)}.`;
+        output.innerText = `You have a budget deficit of ${Math.abs(difference).toFixed(2)}`;
     }
 });
